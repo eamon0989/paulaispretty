fa1d56f559fd65e86766969fd50a0850c1838934
Eamon <eamon0989@gmail.com>
2020-12-07 20:49:48 +0100

    got the random number gen working

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-document.getElementById("paulaBtn").addEventListener("click", displayMsg);
+function rndInt() {
+    let rndInt = Math.floor(Math.random() * 2);
+    console.log(rndInt)
+
+let x = rndInt;
+console.log(x)
 
 const unicorn = document.createElement("img");
 unicorn.setAttribute("src", "unicorn.jpg");
 
-function displayMsg() {
-    document.getElementById("displayMsg").innerHTML = "the most beautiful unicorn in the world!",
-    document.getElementById("displayMsg").appendChild(unicorn);
-}
-    
+const art = document.createElement("img");
+art.setAttribute("src", "artist.gif");    
+
+var msgEl = document.getElementById("displayMsg");
+msgEl.innerHTML = "";
+
+var artEl = document.getElementById("displayArt")
+artEl.innerHTML = "";
+
+if (x === 0) {
+    msgEl.innerHTML = "the most beautiful unicorn in the world!";
+    msgEl.appendChild(unicorn);
+    } else if (x === 1) {
+        msgEl.innerHTML = "an awesome artist";
+        msgEl.appendChild(art);
+    }
+};
